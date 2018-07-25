9982861c963b7777ce9f635251ef18011c1c7f80
Jaime Franklin <franklinjff@gmail.com>
2018-07-25 08:59:59 -0600

    Terraform AWS non autoscale ALB/NLB PAVMSeries

---
+++ WebInDeploy/terraform.tfvars
@@ -0,0 +1,15 @@
+WebCIDR_Block = "10.0.1.0/24"
+
+PublicCIDR_Block = "10.0.0.0/24"
+
+VPCName = "vv-2t-vpc"
+
+StackName = "vv-2t"
+
+VPCCIDR = "10.0.0.0/16"
+
+aws_region = "us-west-2"
+
+ServerKeyName = "jfranklin2"
+
+MasterS3Bucket = "jfgeneric1"
---
+++ WebInFWConf/firewallconfig.tf
@@ -0,0 +1,434 @@
+provider "panos" {
+  hostname = "${var.mgt-ipaddress-fw1}"
+  username = "admin"
+  password = "PaloAlt0!123!!"
+}
+
+provider "panos" {
+  alias    = "fw2"
+  hostname = "${var.mgt-ipaddress-fw2}"
+  username = "admin"
+  password = "PaloAlt0!123!!"
+}
+
+resource "panos_general_settings" "fw1" {
+  hostname    = "WebFW1"
+  dns_primary = "10.0.0.2"
+}
+
+resource "panos_general_settings" "fw2" {
+  provider    = "panos.fw2"
+  hostname    = "WebFW2"
+  dns_primary = "10.0.0.2"
+}
+
+resource "panos_management_profile" "imp_allow_ping" {
+  name = "Allow ping"
+  ping = true
+}
+
+resource "panos_ethernet_interface" "eth1_1" {
+  name               = "ethernet1/1"
+  vsys               = "vsys1"
+  mode               = "layer3"
+  comment            = "External interface"
+  enable_dhcp        = true
+  management_profile = "${panos_management_profile.imp_allow_ping.name}"
+}
+
+resource "panos_ethernet_interface" "eth1_2" {
+  name        = "ethernet1/2"
+  vsys        = "vsys1"
+  mode        = "layer3"
+  comment     = "Web interface"
+  enable_dhcp = true
+}
+
+resource "panos_zone" "zone_untrust" {
+  name       = "UNTRUST"
+  mode       = "layer3"
+  interfaces = ["${panos_ethernet_interface.eth1_1.name}"]
+}
+
+resource "panos_zone" "zone_trust" {
+  name       = "TRUST"
+  mode       = "layer3"
+  interfaces = ["${panos_ethernet_interface.eth1_2.name}"]
+}
+
+resource "panos_service_object" "so_22" {
+  name             = "service-tcp-22"
+  protocol         = "tcp"
+  destination_port = "22"
+}
+
+resource "panos_service_object" "so_221" {
+  name             = "service-tcp-221"
+  protocol         = "tcp"
+  destination_port = "221"
+}
+
+resource "panos_service_object" "so_222" {
+  name             = "service-tcp-222"
+  protocol         = "tcp"
+  destination_port = "222"
+}
+
+resource "panos_service_object" "so_81" {
+  name             = "service-http-81"
+  protocol         = "tcp"
+  destination_port = "81"
+}
+
+resource "panos_address_object" "intNLB" {
+  name        = "AWS-Int-NLB"
+  type        = "fqdn"
+  value       = "${var.int-nlb-fqdn}"
+  description = "AWS Int NLB Address"
+}
+
+resource "panos_security_policies" "security_policies" {
+  rule {
+    name                  = "SSH inbound"
+    source_zones          = ["${panos_zone.zone_untrust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_trust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["ssh", "ping"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "SSH 221-222 inbound"
+    source_zones          = ["${panos_zone.zone_untrust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_trust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["ssh", "ping"]
+    services              = ["${panos_service_object.so_221.name}", "${panos_service_object.so_222.name}"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Allow all ping"
+    source_zones          = ["any"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["any"]
+    destination_addresses = ["any"]
+    applications          = ["ping"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Web browsing"
+    source_zones          = ["${panos_zone.zone_untrust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_trust.name}", "${panos_zone.zone_untrust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["web-browsing"]
+    services              = ["service-http", "${panos_service_object.so_81.name}"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Allow all outbound"
+    source_zones          = ["${panos_zone.zone_trust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_untrust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["any"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Log default deny"
+    source_zones          = ["any"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["any"]
+    destination_addresses = ["any"]
+    applications          = ["any"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "deny"
+  }
+}
+
+resource "panos_nat_policy" "nat1" {
+  name                  = "Web1 SSH"
+  source_zones          = ["${panos_zone.zone_untrust.name}"]
+  destination_zone      = "${panos_zone.zone_untrust.name}"
+  service               = "${panos_service_object.so_221.name}"
+  source_addresses      = ["any"]
+  destination_addresses = ["${var.untrust-ipaddress-fw1}"]
+  sat_type              = "dynamic-ip-and-port"
+  sat_address_type      = "interface-address"
+  sat_interface         = "${panos_ethernet_interface.eth1_2.name}"
+  dat_address           = "10.0.2.50"
+  dat_port              = "22"
+}
+
+resource "panos_nat_policy" "nat2" {
+  name                  = "Web2 SSH"
+  source_zones          = ["${panos_zone.zone_untrust.name}"]
+  destination_zone      = "${panos_zone.zone_untrust.name}"
+  service               = "${panos_service_object.so_222.name}"
+  source_addresses      = ["any"]
+  destination_addresses = ["${var.untrust-ipaddress-fw1}"]
+  sat_type              = "dynamic-ip-and-port"
+  sat_address_type      = "interface-address"
+  sat_interface         = "${panos_ethernet_interface.eth1_2.name}"
+  dat_address           = "10.0.12.50"
+  dat_port              = "22"
+}
+
+resource "panos_nat_policy" "nat3" {
+  name                  = "Webserver1 NAT"
+  source_zones          = ["${panos_zone.zone_untrust.name}"]
+  destination_zone      = "${panos_zone.zone_untrust.name}"
+  service               = "service-http"
+  source_addresses      = ["any"]
+  destination_addresses = ["${var.untrust-ipaddress-fw1}"]
+  sat_type              = "dynamic-ip-and-port"
+  sat_address_type      = "interface-address"
+  sat_interface         = "${panos_ethernet_interface.eth1_2.name}"
+  dat_address           = "${var.nlbprivate-address-fw1}"
+  dat_port              = "80"
+}
+
+resource "panos_virtual_router" "vr1" {
+  name       = "default"
+  interfaces = ["${panos_ethernet_interface.eth1_1.name}", "${panos_ethernet_interface.eth1_2.name}"]
+}
+
+resource "panos_management_profile" "imp_allow_pinga2" {
+  provider = "panos.fw2"
+  name     = "Allow ping"
+  ping     = true
+}
+
+resource "panos_ethernet_interface" "eth1_1a2" {
+  provider           = "panos.fw2"
+  name               = "ethernet1/1"
+  vsys               = "vsys1"
+  mode               = "layer3"
+  comment            = "External interface"
+  enable_dhcp        = true
+  management_profile = "${panos_management_profile.imp_allow_ping.name}"
+}
+
+resource "panos_ethernet_interface" "eth1_2a2" {
+  provider    = "panos.fw2"
+  name        = "ethernet1/2"
+  vsys        = "vsys1"
+  mode        = "layer3"
+  comment     = "Web interface"
+  enable_dhcp = true
+}
+
+resource "panos_zone" "zone_untrusta2" {
+  provider   = "panos.fw2"
+  name       = "UNTRUST"
+  mode       = "layer3"
+  interfaces = ["${panos_ethernet_interface.eth1_1.name}"]
+}
+
+resource "panos_zone" "zone_trusta2" {
+  provider   = "panos.fw2"
+  name       = "TRUST"
+  mode       = "layer3"
+  interfaces = ["${panos_ethernet_interface.eth1_2.name}"]
+}
+
+resource "panos_service_object" "so_22a2" {
+  provider         = "panos.fw2"
+  name             = "service-tcp-22"
+  protocol         = "tcp"
+  destination_port = "22"
+}
+
+resource "panos_service_object" "so_221a2" {
+  provider         = "panos.fw2"
+  name             = "service-tcp-221"
+  protocol         = "tcp"
+  destination_port = "221"
+}
+
+resource "panos_service_object" "so_222a2" {
+  provider         = "panos.fw2"
+  name             = "service-tcp-222"
+  protocol         = "tcp"
+  destination_port = "222"
+}
+
+resource "panos_service_object" "so_81a2" {
+  provider         = "panos.fw2"
+  name             = "service-http-81"
+  protocol         = "tcp"
+  destination_port = "81"
+}
+
+resource "panos_address_object" "intNLB2" {
+  provider = "panos.fw2"
+  name     = "AWS-Int-NLB"
+  type     = "fqdn"
+  value    = "${var.int-nlb-fqdn}"
+}
+
+resource "panos_security_policies" "security_policiesa2" {
+  provider = "panos.fw2"
+
+  rule {
+    name                  = "SSH inbound"
+    source_zones          = ["${panos_zone.zone_untrust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_trust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["ssh", "ping"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "SSH 221-222 inbound"
+    source_zones          = ["${panos_zone.zone_untrust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_trust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["ssh", "ping"]
+    services              = ["${panos_service_object.so_221.name}", "${panos_service_object.so_222.name}"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Allow all ping"
+    source_zones          = ["any"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["any"]
+    destination_addresses = ["any"]
+    applications          = ["ping"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Web browsing"
+    source_zones          = ["${panos_zone.zone_untrust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_trust.name}", "${panos_zone.zone_untrust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["web-browsing"]
+    services              = ["service-http", "${panos_service_object.so_81.name}"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Allow all outbound"
+    source_zones          = ["${panos_zone.zone_trust.name}"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["${panos_zone.zone_untrust.name}"]
+    destination_addresses = ["any"]
+    applications          = ["any"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+  }
+
+  rule {
+    name                  = "Log default deny"
+    source_zones          = ["any"]
+    source_addresses      = ["any"]
+    source_users          = ["any"]
+    hip_profiles          = ["any"]
+    destination_zones     = ["any"]
+    destination_addresses = ["any"]
+    applications          = ["any"]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "deny"
+  }
+}
+
+resource "panos_nat_policy" "nat1a2" {
+  provider              = "panos.fw2"
+  name                  = "Web1 SSH"
+  source_zones          = ["${panos_zone.zone_untrust.name}"]
+  destination_zone      = "${panos_zone.zone_untrust.name}"
+  service               = "${panos_service_object.so_221.name}"
+  source_addresses      = ["any"]
+  destination_addresses = ["${var.untrust-ipaddress-fw2}"]
+  sat_type              = "dynamic-ip-and-port"
+  sat_address_type      = "interface-address"
+  sat_interface         = "${panos_ethernet_interface.eth1_2.name}"
+  dat_address           = "10.0.2.50"
+  dat_port              = "22"
+}
+
+resource "panos_nat_policy" "nat2a2" {
+  provider              = "panos.fw2"
+  name                  = "Web2 SSH"
+  source_zones          = ["${panos_zone.zone_untrust.name}"]
+  destination_zone      = "${panos_zone.zone_untrust.name}"
+  service               = "${panos_service_object.so_222.name}"
+  source_addresses      = ["any"]
+  destination_addresses = ["${var.untrust-ipaddress-fw2}"]
+  sat_type              = "dynamic-ip-and-port"
+  sat_address_type      = "interface-address"
+  sat_interface         = "${panos_ethernet_interface.eth1_2.name}"
+  dat_address           = "10.0.12.50"
+  dat_port              = "22"
+}
+
+resource "panos_nat_policy" "nat3a2" {
+  provider              = "panos.fw2"
+  name                  = "Webserver1 NAT"
+  source_zones          = ["${panos_zone.zone_untrust.name}"]
+  destination_zone      = "${panos_zone.zone_untrust.name}"
+  service               = "service-http"
+  source_addresses      = ["any"]
+  destination_addresses = ["${var.untrust-ipaddress-fw2}"]
+  sat_type              = "dynamic-ip-and-port"
+  sat_address_type      = "interface-address"
+  sat_interface         = "${panos_ethernet_interface.eth1_2.name}"
+  dat_address           = "${var.nlbprivate-address-fw2}"
+  dat_port              = "80"
+}
+
+resource "panos_virtual_router" "vr1a2" {
+  provider   = "panos.fw2"
+  name       = "default"
+  interfaces = ["${panos_ethernet_interface.eth1_1.name}", "${panos_ethernet_interface.eth1_2.name}"]
+}
